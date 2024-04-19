2567e9cde744683b8a92dc29e14e1d11bf6d6f2f
mrusse <99michaelrussell99@gmail.com>
2024-04-18 22:16:38 -0400

    processing songs into album,artist,song structure

--- music-tagger.py
+++ music-tagger.py
@@ -7,6 +7,8 @@ parser.add_argument('-d',type=str, required=True)
 args = parser.parse_args()
 
 songs = []
+artists = {}
+albums = {}
 
 for path, subdirs, files in os.walk(args.d):
     for name in files:
@@ -14,8 +16,26 @@ for path, subdirs, files in os.walk(args.d):
             file = music_tag.load_file(os.path.join(path, name))
             songs.append(file)
         except:
-            print("An exception occurred with file: " + os.path.join(path, name)) 
+            print("Skipping processing of non-music file: " + os.path.join(path, name)) 
 
 for song in songs:
-    title_item = song['title']
-    print(str(title_item))
+    album = song['album']
+    if(str(album) not in albums):
+        albums[str(album)] = [song]
+    else:
+        albums[str(album)].append(song)
+
+for key in albums:
+    artist = albums[key][0]['artist']
+    if(str(artist) not in artists):
+        artists[str(artist)] = [albums[key]]
+    else:
+        artists[str(artist)].append(albums[key])
+
+for artist in artists:
+    for album in artists[artist]:
+        for song in album:
+            song_artist = song['artist']
+            song_album  = song['album']
+            song_title  = song['title']
+            print(str(song_artist) + " - " + str(song_album) + " - " + str(song_title))
