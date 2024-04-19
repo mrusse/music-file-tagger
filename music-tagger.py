282aae9080d94caf12e4eb8be297920bea5566cb
mrusse <99michaelrussell99@gmail.com>
2024-04-18 21:25:21 -0400

    getting all songs from a directory into a list

--- music-tagger.py
+++ music-tagger.py
@@ -1,6 +1,21 @@
 import music_tag
+import argparse
+import os
 
-file = music_tag.load_file("02. Shimmering Stars - I'm Gonna Try.flac")
-title_item = file['title']
+parser = argparse.ArgumentParser(description='music-tagger')
+parser.add_argument('-d',type=str, required=True)
+args = parser.parse_args()
 
-print(str(title_item))
+songs = []
+
+for path, subdirs, files in os.walk(args.d):
+    for name in files:
+        try:
+            file = music_tag.load_file(os.path.join(path, name))
+            songs.append(file)
+        except:
+            print("An exception occurred with file: " + os.path.join(path, name)) 
+
+for song in songs:
+    title_item = song['title']
+    print(str(title_item))
