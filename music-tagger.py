46886f7f722661423e59bd5bc6267f706ecbba2a
mrusse <99michaelrussell99@gmail.com>
2024-04-19 19:08:30 -0400

    added comments since i have already gotten confused at my goals here...

--- music-tagger.py
+++ music-tagger.py
@@ -2,12 +2,14 @@ import music_tag
 import argparse
 import os
 
+#Track number padder (turns "1" to "01") 
 def pad_track_num(num):
   if(len(str(num)) < 2):
       return "0" + str(num)
   else:
       return str(num)
   
+#Song to string. TODO: add more data to this   
 def song_to_string(song):
     tracknumber  = song.raw['tracknumber'].value
     artist       = song['artist']
@@ -29,22 +31,28 @@ illegalChars = ["\\","/",":","*","?",'"',"<",">","|"]
 for path, subdirs, files in os.walk(args.d):
     for name in files:
         try:
+            #Get song from file
             song = music_tag.load_file(os.path.join(path, name))
+
+            #Basic editing of metadata (pad tracknumber and remove discnumber)
             song.raw['tracknumber'] = pad_track_num(song['tracknumber'])
             song.remove_tag('discnumber')
             songs.append(song)
             song.save()
 
+            #Add songs to albums
             album = song['album']
             if(str(album) not in albums):
                 albums[str(album)] = [song]
             else:
                 albums[str(album)].append(song)
 
+            #Edit song filenames to %tracknumber% %title% structure
             tracknumber  = song.raw['tracknumber'].value
             title        = song['title']
             filename = str(tracknumber) + " " + str(title) + ".flac"
 
+            #Check filename for illegal chars that can sneak in from track title
             for c in illegalChars:
                 if c in filename:
                     filename = filename.replace(c,"")
@@ -53,6 +61,7 @@ for path, subdirs, files in os.walk(args.d):
         except Exception as e:
             print("Skipping processing of non-music file: " + os.path.join(path, name)) 
 
+#Add albums to artists. Might not actually need this or the albums...
 for key in albums:
     artist = albums[key][0]['artist']
     if(str(artist) not in artists):
