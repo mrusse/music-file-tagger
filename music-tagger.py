b1e28a5844104b3cdc4e0ce08fb9c9e04a5a2bd9
mrusse <99michaelrussell99@gmail.com>
2024-04-19 23:30:16 -0400

    Folder naming and file artwork

--- music-tagger.py
+++ music-tagger.py
@@ -1,6 +1,9 @@
+from get_cover_art import CoverFinder
 import music_tag
 import argparse
+import shutil
 import os
+import time
 
 #Track number padder (turns "1" to "01") 
 def pad_track_num(num):
@@ -17,7 +20,9 @@ def song_to_string(song):
     title        = song['title']
     discnumber   = song['discnumber']
     return(str(artist) + " - " + str(album) + " - " + str(tracknumber) + " " + str(title) + " - " + str(discnumber))
-      
+
+start_time = time.time()
+
 parser = argparse.ArgumentParser(description='music-tagger')
 parser.add_argument('-d',type=str, required=True)
 args = parser.parse_args()
@@ -26,6 +31,7 @@ songs = []
 artists = {}
 albums = {}
 
+dirsToRemove = []
 illegalChars = ["\\","/",":","*","?",'"',"<",">","|"]
 
 for path, subdirs, files in os.walk(args.d):
@@ -50,21 +56,42 @@ for path, subdirs, files in os.walk(args.d):
             #Edit song filenames to %tracknumber% %title% structure
             tracknumber  = song.raw['tracknumber'].value
             title        = song['title']
+            artist       = str(song['artist'])
+            album        = str(song['album'])
             filename = str(tracknumber) + " " + str(title) + ".flac"
-
+            
             #Check filename for illegal chars that can sneak in from track title
             for c in illegalChars:
                 if c in filename:
                     filename = filename.replace(c,"")
+                if c in artist:
+                    artist = artist.replace(c,"")                    
+                if c in album:
+                    album = album.replace(c,"")
 
-            os.rename(os.path.join(path, name), os.path.join(path, filename))
+            #Make "Artist\Album" folders if they don't exist
+            if not os.path.isdir(os.path.join(args.d, artist)):
+                os.mkdir(os.path.join(args.d, artist))
+                os.mkdir(os.path.join(args.d + "\\" + artist, album))
+            elif not os.path.isdir(os.path.join(args.d + "\\" + artist, album)):
+                os.mkdir(os.path.join(args.d + "\\" + artist, album))
+
+            if(path not in dirsToRemove):
+                dirsToRemove.append(path)
+                
+            #Rename and move processed files
+            os.rename(os.path.join(path, name), os.path.join(args.d + "\\" + artist + "\\" + album, filename))
         except Exception as e:
             print("Skipping processing of non-music file: " + os.path.join(path, name)) 
 
-#Add albums to artists. Might not actually need this or the albums...
-for key in albums:
-    artist = albums[key][0]['artist']
-    if(str(artist) not in artists):
-        artists[str(artist)] = [albums[key]]
-    else:
-        artists[str(artist)].append(albums[key])
+#Remove old dirs
+for path in dirsToRemove:
+    try:
+        shutil.rmtree(path)
+    except Exception as e:
+        print(e)
+
+finder = CoverFinder({'art-size': 600, 'cleanup': True, 'clear': True})
+finder.scan_folder(args.d)
+
+print("--- %s seconds ---" % (time.time() - start_time))
