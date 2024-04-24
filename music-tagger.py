3e4b90d98dac4db9e8316c8dbb7be669b413490b
mrusse <99michaelrussell99@gmail.com>
2024-04-24 15:54:57 -0400

    User can now manually give cover image if program fails to find one

--- music-tagger.py
+++ music-tagger.py
@@ -1,4 +1,5 @@
 import os
+import io
 import sys
 import time
 import shutil
@@ -6,6 +7,7 @@ from io import StringIO
 
 import argparse
 import music_tag
+from PIL import Image
 from tqdm import tqdm
 from pydub import AudioSegment
 from pydub.utils import mediainfo
@@ -17,7 +19,14 @@ def pad_track_num(num):
       return "0" + num
   else:
       return num
-  
+
+#Pillow image to byte array
+def image_to_byte_array(image: Image) -> bytes:
+  imgByteArr = io.BytesIO()
+  image.save(imgByteArr, format=image.format)
+  imgByteArr = imgByteArr.getvalue()
+  return imgByteArr
+
 #Song to string. TODO: add more data to this   
 def song_to_string(song):
     tracknumber  = song.raw['tracknumber'].value
@@ -30,13 +39,16 @@ def song_to_string(song):
 start_time = time.time()
 
 parser = argparse.ArgumentParser(description='music-tagger')
-parser.add_argument('-d',type=str, required=True)
+parser.add_argument('-d',type=str, required=True, help="directory where your music is stored")
+parser.add_argument('-a',action="store_true", help="If enabled, will use provided cover art (placed in the directory supplied with -d) when downloading the art fails.")
 args = parser.parse_args()
 
 songs = []
+albums = {}
+last_flac = ""
 
-dirsToRemove = []
-illegalChars = ["\\","/",":","*","?",'"',"<",">","|"]
+dirs_to_remove = []
+illegal_chars = ["\\","/",":","*","?",'"',"<",">","|"]
 finder = CoverFinder({'art-size': 600, 'cleanup': True, 'clear': True, 'verbose': False})
 s = StringIO()
 
@@ -47,8 +59,13 @@ for path, subdirs, files in os.walk(args.d):
     if(songs == []):
         print ("\033[A                             \033[A")
 
-    songList = tqdm(files, bar_format='{desc}{percentage:3.0f}% |{bar}| {n_fmt}/{total_fmt}')
-    for name in songList:
+    song_list = tqdm(files, bar_format='{desc}{percentage:3.0f}% |{bar}| {n_fmt}/{total_fmt}')
+
+    for name in files:
+        if(name.split(".")[-1] == "flac"):
+            last_flac = name
+
+    for name in song_list:
         try:
             #Get song from file
             song = music_tag.load_file(os.path.join(path, name))
@@ -57,7 +74,7 @@ for path, subdirs, files in os.walk(args.d):
             album        = str(song['album'])
 
             #Progress bar
-            songList.set_description("Applying metadata for: " + artist + " - " + album + " - " + str(title))
+            song_list.set_description("Applying metadata for: " + artist + " - " + album + " - " + str(title))
 
             #Basic editing of metadata (pad tracknumber and remove discnumber)
             raw_tracknum = song.raw['tracknumber'].value.split("\\")[0].split("/")[0]
@@ -73,7 +90,7 @@ for path, subdirs, files in os.walk(args.d):
             mp3_location = args.d + "\\" + "Music (320)"
         
             #Check filename for illegal chars that can sneak in from track title
-            for c in illegalChars:
+            for c in illegal_chars:
                 if c in filename:
                     filename = filename.replace(c,"")
                 if c in artist:
@@ -86,11 +103,11 @@ for path, subdirs, files in os.walk(args.d):
                 os.makedirs(os.path.join(flac_location, artist, album))
                 os.makedirs(os.path.join(mp3_location, artist, album))
                 
-            if(path not in dirsToRemove):
-                dirsToRemove.append(path)
+            if(path not in dirs_to_remove):
+                dirs_to_remove.append(path)
 
             #Progress bar
-            songList.set_description("Converting flac > mp3: " + artist + " - " + album + " - " + str(title))
+            song_list.set_description("Converting flac > mp3: " + artist + " - " + album + " - " + str(title))
 
             #Convert flacs to mp3's then rename and move processed files
             flac_path = os.path.join(path, name)
@@ -110,6 +127,13 @@ for path, subdirs, files in os.walk(args.d):
             os.rename(flac_path, os.path.join(flac_location, artist, album, filename))
             os.rename(os.path.join(path, mp3_old), os.path.join(mp3_location, artist, album, mp3_filename))
 
+            #Add songs to albums
+            if(str(song['album']) not in albums):
+                albums[str(song['album'])] = [os.path.join(flac_location, artist, album, filename),os.path.join(mp3_location, artist, album, mp3_filename)]
+            else:
+                albums[str(song['album'])].append(os.path.join(flac_location, artist, album, filename))
+                albums[str(song['album'])].append(os.path.join(mp3_location, artist, album, mp3_filename))
+
             #Remove discnumber if it failed to do so before (sometimes gets added back to the mp3 somehow)
             new_mp3 = music_tag.load_file(os.path.join(mp3_location, artist, album, mp3_filename))
             if(str(new_mp3['discnumber']) != ""):
@@ -117,17 +141,46 @@ for path, subdirs, files in os.walk(args.d):
                 new_mp3.save()
 
             #Progress bar
-            songList.set_description("Embedding cover image: " + artist + " - " + album + " - " + str(title))
+            song_list.set_description("Searching cover image: " + artist + " - " + album + " - " + str(title))
 
-            #Grab artwork      
+            #Grab artwork    
             sys.stdout = s
             finder.scan_file(os.path.join(flac_location, artist, album, filename))
             finder.scan_file(os.path.join(mp3_location, artist, album, mp3_filename))
             sys.stdout = sys.__stdout__
+            
+            #Check if the finder couldn't get the album cover. Then wait for the user to supply one.
+            if(os.path.join(flac_location, artist, album, filename) in finder.files_skipped and name == last_flac and args.a):
+                #Wait for user to copy cover.jpg to folder
+                while not any(fname.endswith('.jpg') for fname in os.listdir(args.d)):
+                    #Progress bar
+                    song_list.set_description("COVER IMAGE NOT FOUND: " + artist + " - " + album)
+
+                #Let the file copy
+                time.sleep(1)
+
+                #Resize cover image   
+                cover_name = args.d + '\\cover.jpg'
+                orig_image = Image.open(cover_name)
+                orig_image = orig_image.resize((600,600))
+                orig_image = orig_image.save(args.d + '\\resized.jpg')
+                os.remove(cover_name) 
+
+                #Convert image to byte array
+                cover_name = args.d + '\\resized.jpg'
+                cover_image = image_to_byte_array(Image.open(cover_name))
+
+                #Embed image to each song on the album
+                for song_title in albums[str(song['album'])]:
+                    song = music_tag.load_file(song_title)
+                    song['artwork'] =  cover_image
+                    song.save()
+
+                os.remove(cover_name) 
 
         except NotImplementedError:
             #Progress bar
-            songList.set_description("Skipped non-flac file: " + name)
+            song_list.set_description("Skipped non-flac file: " + name)
             pass
         except Exception as e:
             print(e)
@@ -135,23 +188,24 @@ for path, subdirs, files in os.walk(args.d):
 
         if(name == files[-1]):
             #Progress bar    
-            songList.set_description("Finished processing!!: " + artist + " - " + album)
+            song_list.set_description("Finished processing!!: " + artist + " - " + album)
 
 #Remove old dirs
-for path in dirsToRemove:
+for path in dirs_to_remove:
     try:
         shutil.rmtree(path)
     except Exception as e:
         print(e)
 
 #Print out files that we missed grabbing artwork for
-if finder.files_skipped != []:
-    print("\nSkipped grabbing artwork for these files: ")
-for file in finder.files_skipped:
-    split = file.split("\\")
-    title = split[-1]
-    album = split[-2]
-    artist = split[-3]
-    print(artist + " - " + album + " - " + title)
+if not args.a:
+    if finder.files_skipped != []:
+        print("\nSkipped grabbing artwork for these files: ")
+    for file in finder.files_skipped:
+        split = file.split("\\")
+        title = split[-1]
+        album = split[-2]
+        artist = split[-3]
+        print(artist + " - " + album + " - " + title)
 
 print("\n♪♪♪♪♪♪♪♪♪♪ Finished in: %s seconds! ♪♪♪♪♪♪♪♪♪♪" % '{0:.2f}'.format(time.time() - start_time))
